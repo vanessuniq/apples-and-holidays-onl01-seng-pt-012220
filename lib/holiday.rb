fb364d13bffdcb08a02fb4b15299487d2a90eaf6
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 06:23:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |supply|
       puts season.capitalize! + ":"
-      puts "  #{holiday}: "
+      puts "  #{holiday}: #{supply}"
     end 
     
     
