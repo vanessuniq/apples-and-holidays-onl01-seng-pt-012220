f898e15462becad9a68f4f2ab2c0f5cd85ec9ab9
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 05:59:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_supplies[:winter]
+  holiday_supplies[:winter].values.flatten
 
 end
 
