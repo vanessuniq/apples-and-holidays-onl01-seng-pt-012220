c9dadbf46b586a688618d3356f8cf193710186a7
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 05:55:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
-  holiday_supplies[:winter]
+  holiday_supplies[:winter][:christmas_eve]
   # remember to return the updated hash
 
 end
