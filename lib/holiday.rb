871494505725ef221f761db98a7de68c31945ca3
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 05:51:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,6 +36,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   holiday_supplies[:spring][:memorial_day] << "Ribs"
+  holiday_supplies
 
 end
 
