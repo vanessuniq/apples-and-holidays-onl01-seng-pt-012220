14b5400e3473d64d227f9780254494e9eecb4677
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 06:12:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season
+    puts season + ":"
+    
   end 
 
 end
