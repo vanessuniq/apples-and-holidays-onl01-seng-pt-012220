4cd60143f0fc7c0d7cc248eeb1f05cfbab714e1b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 06:41:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    
+    holiday.each do |supply|
+      
+    end 
   end 
   
 
