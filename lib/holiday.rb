394bc0edc4ab742d0e54196beb964e24f2f04a9e
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-03 06:19:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    holiday.each do ||
+    holiday.each do |supply|
     puts season.capitalize! + ":"
     
   end 
